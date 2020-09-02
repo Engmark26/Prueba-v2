b4423d45cfd78476deeec41bbd6c8bf65379d5fd
MARCOS JAVIER PALOMO CABRAL <I20050027@monclova.tecnm.mx>
2020-09-02 14:34:41 -0500

    Proyecto copiado con C#

---
+++ ProyectoPrueba/ProyectoPrueba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Helloworld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Cadena;
+            int DatoEntero;
+            double DatoReal;
+            Console.Write("¿Cual es tu nombre?: ");
+            Cadena = Console.ReadLine();
+            Console.Write("¿Cual es tu edad?: ");
+            DatoEntero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce el precio de unos cheetos: ");
+            DatoReal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tu nombre: " + Cadena);
+            Console.WriteLine("Tu edad: " + DatoEntero.ToString());
+            Console.WriteLine("Barra pan: " + DatoReal.ToString("0.00"));
+
+        }
+
+    }
+}
